379cc1a829aa328c45344806b663ebefd4fd0f07
hisabrakhoki <hisabrakhoki@gmail.com>
2023-03-22 15:50:24 +0600

    monfdk

---
+++ letconst.js
@@ -0,0 +1,31 @@
+const hubby = "Eleas kanchon"
+console.log(hubby);
+
+const number = [12, 15, 25]
+number[2]=45;
+number.push(63);
+// number = [12, 22, 1, 2, 336]
+console.log(number); 
+
+const friendName = ["Bulbul", "parvez", "Sajib"]
+friendName[1]="Rasel";
+console.log(friendName);
+
+const nayok = {name:"shakib Khan", phoneNumber:"01872022514", addres:"katabon"}
+nayok.name="salman khan"
+console.log(nayok)
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala"
+console.log(patientName);
+
+sum = 0;
+// for(var i = 0; i<20; i++){
+//     sum = sum + i;
+// }
+// console.log(i);
+
+for(let i = 0; i<10; i++){
+    sum = sum + i;
+}
+console.log(i);
---
+++ template.js
@@ -0,0 +1,16 @@
+ const firstName = "Justin";
+ const lastName = "TimberLake";
+//  const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName}`
+ console.log(fullName2);
+
+ const multiline = "I love you\n"
+    +"I miss you\n"
+    +"I need you\n";
+console.log(multiline);
+
+const multiline2 = `I love you
+I miss you
+NO. I don't need you
+Baily road e dorkar nai`
+console.log(multiline2);
